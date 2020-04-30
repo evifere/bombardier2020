397d6fe1abb075fd4ca569a95ddea92f03b29156
JOLLY <evifere@gmail.com>
2020-04-30 13:39:19 +0200

    add blue canvas

--- app/src/components/Game.vue
+++ app/src/components/Game.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>game</div>
+  <el-container>
+    <el-header>game score</el-header>
+    <el-main>
+      <canvas id="screen" tabindex="0"></canvas>
+    </el-main>
+    <el-footer align="right">copyright 2020 bombardier2020 v{{version}}</el-footer>
+  </el-container>
 </template>
 
 <script>
+import { fabric } from "fabric";
+import { version } from "../../package.json";
+
 export default {
   name: "Game",
 
+  data() {
+    return {
+      version
+    };
+  },
   mounted() {
-    console.log("mounted");
+    this.$canvas = new fabric.Canvas("screen", {
+      width: 1100,
+      height: 700,
+      backgroundColor: "blue"
+    });
   }
 };
 </script>
+
+<style scoped>
+.el-main {
+  margin-left: 20%;
+}
+</style>
