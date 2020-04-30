ed0f22d5a146b3826607f812c2b2f735fe7cad40
JOLLY <evifere@gmail.com>
2020-04-30 14:02:25 +0200

    display plane

--- app/src/components/Game.vue
+++ app/src/components/Game.vue
@@ -21,11 +21,34 @@ export default {
     };
   },
   mounted() {
-    this.$canvas = new fabric.Canvas("screen", {
+    let _self = this;
+
+    this.$canvas = new fabric.StaticCanvas("screen", {
       width: 1100,
       height: 700,
       backgroundColor: "blue"
     });
+
+    fabric.Image.fromURL(this.getBaseUrl() + "plane-96.png", function(oImg) {
+      oImg.set("left", 10).set("top", 10);
+      _self.$canvas.add(oImg);
+      _self.$plane = oImg;
+    });
+  },
+
+  methods: {
+    getBaseUrl() {
+      let baseUrl =
+        process.env.NODE_ENV === "production"
+          ? "http://evifere.lescigales.org/bombardier2020/img/"
+          : "./img/";
+
+      if (window.location.hostname === "evifere.github.io") {
+        baseUrl = "https://evifere.github.io/bombardier2020/img/";
+      }
+
+      return baseUrl;
+    }
   }
 };
 </script>
