cd0cf9392421bf552fa2ca2092a4db50e8c341d0
JOLLY <evifere@gmail.com>
2020-04-30 16:38:28 +0200

    launch one bomb

--- app/src/components/Game.vue
+++ app/src/components/Game.vue
@@ -18,7 +18,9 @@ export default {
   data() {
     return {
       version,
-      count: 0
+      count: 0,
+      countBomb: 0,
+      bombLaunched: false
     };
   },
   mounted() {
@@ -34,8 +36,10 @@ export default {
       oImg.set("left", 10).set("top", 10);
       _self.$canvas.add(oImg);
       _self.$plane = oImg;
-      _self.animatePlane(0);
+      _self.animatePlane();
     });
+
+    window.addEventListener("mouseup", this.onLaunchBomb);
   },
 
   methods: {
@@ -69,6 +73,39 @@ export default {
       this.$plane.set("left", 10 + this.$data.count * 10);
       canvas.renderAll();
       setTimeout(this.animatePlane, 100);
+    },
+
+    createBomb(left, top) {
+      let _self = this;
+
+    fabric.Image.fromURL(this.getBaseUrl() + "bomb-40.png", function(oImg) {
+        oImg.set("left", left).set("top", top);
+        _self.$canvas.add(oImg);
+        _self.$bomb = oImg;
+        _self.animateBomb(0);
+      });
+    },
+
+    animateBomb() {
+      let canvas = this.$canvas;
+
+      this.$data.countBomb++;
+      if (this.$bomb.get("top") > 500) {
+        this.$data.bombLaunched = false;
+        this.$data.countBomb = 0;
+        return false;
+      }
+
+      this.$bomb.set("top", this.$bomb.get("top") + this.$data.countBomb * 10);
+      canvas.renderAll();
+      setTimeout(this.animateBomb, 250);
+    },
+    onLaunchBomb() {
+      if (this.$data.bombLaunched === true) {
+        return false;
+      }
+      this.$data.bombLaunched = true;
+      this.createBomb(this.$plane.get("left"), this.$plane.get("top") + 10);
     }
   }
 };
